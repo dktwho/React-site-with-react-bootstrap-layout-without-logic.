b2a6d978e9bcaea8bde10b6033857360ae94d6db
Alexander Korobov <dktwho@mail.ru>
2023-03-03 18:48:34 +0300

    add feedback form on page Contacts

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { Form, Button, Container } from 'react-bootstrap'
 
 const Contacts = () => {
   return (
-    <div>Contacts</div>
+
+    <Container style={{width: '500px'}}>
+      <h1 className='text-center'>Contact us</h1>
+      <Form>
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Label>Email address</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+      <Form.Text className="text-muted">
+        We'll never share your email with anyone else.
+      </Form.Text>
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Label>Example textarea</Form.Label>
+      <Form.Control as="textarea" rows="3" />
+    </Form.Group>
+    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+      <Form.Check type="checkbox" label="Check me out" />
+    </Form.Group>
+    <Button variant="dark" type="submit">
+      Submit
+    </Button>
+  </Form>
+    </Container>
+   
   )
 }
 
